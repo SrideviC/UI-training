31a4385ebf69b2d7ae1a229f590b74a7b4a3c7ba
SrideviC <30946210+SrideviC@users.noreply.github.com>
2021-03-19 13:00:38 +0530

    Add files via upload

---
+++ User-Survey-Management/src/app/Services/user-registration.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRegistrationService {
+
+  constructor(private http:HttpClient) { }
+
+
+  public doRegistration(user){
+    return this.http.post("http://localhost:9090/user/signup",user,{responseType:'text' as 'json'});
+  }
+
+  public doUserSurvey(survey){
+    return this.http.post("http://localhost:9090/survey",survey);
+  }
+
+
+}
